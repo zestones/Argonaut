c6c2a78eae0c5c48bca21dbc10d20abc2ef1e15c
zestones <idrissbenguezzou@gmail.com>
2024-12-13 18:19:43 +0100

    fix: refactor idf scope redefinition

--- src/semantic_analysis/scope_checks/scope_validation.c
+++ src/semantic_analysis/scope_checks/scope_validation.c
@@ -4,8 +4,13 @@
 void check_scope_redefinition(int index_lexeme_lexicographic, char *entity_type) {
     if (is_identifier_already_tracked(index_lexeme_lexicographic)) {
         set_error_type(&error, SEMANTIC_ERROR);
-        set_error_message(&error, "Identifier '%s' is already defined in the current scope as a %s.", get_lexeme(index_lexeme_lexicographic), entity_type);
-
+        set_error_message(
+            &error,
+            "Identifier '%s' is already defined in the current scope as a %s.\n"
+            "  Consider renaming the identifier or modifying the existing definition.\n",
+            get_lexeme(index_lexeme_lexicographic),
+            entity_type
+        );
         yerror(error);
     }
 }
